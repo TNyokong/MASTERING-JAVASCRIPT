3b1dfb8fdeb259ebf7a4e1357767f9423ac5cbb2
TNyokong <126710714+TNyokong@users.noreply.github.com>
2024-09-05 20:38:01 +0200

    Notes

---
+++ index.js
@@ -0,0 +1,83 @@
+//Variables
+
+/*let lebitso = 'Thapelo';
+console.log(lebitso);
+
+
+const interestRate = 0.3;
+console.log(interestRate);
+
+*/
+
+//Primitives/Value Types
+// 1. String
+// 2. Number
+// 3. Boolean
+// 4. Undefined
+// 5. null  
+
+//EXAMPLES
+//let name = "Thapelo";   // String Literal
+//let age = 30;           // Number Literal
+//isApproved = false;     // Bolean Literal
+//firstName = undefined;   
+//letSelector = null;    
+
+//Using typeof in the console to check the data type
+
+
+//Reference Types
+
+//1. Object
+//2. Array
+//3. Function
+
+//EXAMPLES
+
+/*
+let person ={
+    name: "Thapelo",
+    age: "27",
+}
+*/
+
+//Dot Notation
+//person.name = 'TP';
+
+//OR
+
+//Bracket Notation
+//person['name] = 'TP'
+
+//ARRAYS
+
+//let selectedColors = ['red','blue'];
+//selectedColors[2] = 'green';
+//console.log(selectedColors[0]);
+//console.log(selectedColors.length)
+//Array is an object in Javascript
+
+//Array is a type of data structure used to present a list of items
+ 
+//FUNCTIONS
+
+/*
+function greet(name){
+    console.log('Hello' + name);
+
+
+    greet(John);
+
+You pass the argument to the parameter
+
+
+
+*/
+
+
+
+
+
+
+
+ 
